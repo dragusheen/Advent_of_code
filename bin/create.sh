683b237dc598fdcad5e22984f3de0c0d3359b0bb
dragusheen <nathan.tirolf@epitech.eu>
2025-12-04 16:39:31 -0500

    [+] day_15 2015

--- bin/create.sh
+++ bin/create.sh
@@ -47,8 +47,7 @@ if [ ! -f "./src/$YEAR/$DAY" ]; then
     echo "    pass" >> ./src/$YEAR/day_$DAY
     echo "" >> ./src/$YEAR/day_$DAY
     echo "if __name__ == \"__main__\":" >> ./src/$YEAR/day_$DAY
-    echo "    content = get_file_content(DATA_PATH)" >> ./src/$YEAR/day_$DAY
-    echo "    content = content.split(\"\n\")" >> ./src/$YEAR/day_$DAY
+    echo "    content = [row for row in get_file_content(DATA_PATH).split(\"\n\") if row]" >> ./src/$YEAR/day_$DAY
     echo "    print(f\"Part 1: {part_1(content)}\")" >> ./src/$YEAR/day_$DAY
     echo "    print(f\"Part 2: {part_2(content)}\")" >> ./src/$YEAR/day_$DAY
 
